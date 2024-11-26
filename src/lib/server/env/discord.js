7f9c07bf6ef9380ee106bbd8f3bcd362bed96b29
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-26 21:01:51 +0800

    fix(env): only assert values in production

--- src/lib/server/env/discord.js
+++ src/lib/server/env/discord.js
@@ -1,12 +1,15 @@
+import { building, dev } from '$app/environment';
+
 import { Buffer } from 'node:buffer';
 import assert from 'node:assert/strict';
 import { env } from '$env/dynamic/private';
 
-assert(typeof env.DISCORD_APPLICATION_ID === 'string', 'missing discord application id');
-export const DISCORD_APPLICATION_ID = env.DISCORD_APPLICATION_ID;
+if (!building && !dev) {
+    assert(typeof env.DISCORD_APPLICATION_ID === 'string', 'missing discord application id');
+    assert(typeof env.DISCORD_PUBLIC_KEY === 'string', 'missing discord public key');
+    assert(typeof env.DISCORD_BOT_TOKEN === 'string', 'missing discord bot token');
+}
 
-assert(typeof env.DISCORD_PUBLIC_KEY === 'string', 'missing discord public key');
-export const DISCORD_PUBLIC_KEY = Buffer.from(env.DISCORD_PUBLIC_KEY, 'hex');
-
-assert(typeof env.DISCORD_BOT_TOKEN === 'string', 'missing discord bot token');
-export const DISCORD_BOT_TOKEN = env.DISCORD_BOT_TOKEN;
+export const DISCORD_APPLICATION_ID = env.DISCORD_APPLICATION_ID ?? '';
+export const DISCORD_PUBLIC_KEY = Buffer.from(env.DISCORD_PUBLIC_KEY ?? '', 'hex');
+export const DISCORD_BOT_TOKEN = env.DISCORD_BOT_TOKEN ?? '';
--- src/lib/server/env/postgres.js
+++ src/lib/server/env/postgres.js
@@ -1,5 +1,8 @@
+import { building, dev } from '$app/environment';
+
 import assert from 'node:assert/strict';
 import { env } from '$env/dynamic/private';
 
-assert(typeof env.POSTGRES_DATABASE_URL === 'string', 'missing postgres database url');
-export const POSTGRES_DATABASE_URL = env.POSTGRES_DATABASE_URL;
+if (!building && !dev) assert(typeof env.POSTGRES_DATABASE_URL === 'string', 'missing postgres database url');
+
+export const POSTGRES_DATABASE_URL = env.POSTGRES_DATABASE_URL ?? '';
