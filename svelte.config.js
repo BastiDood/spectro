0b6d55d63c2a6b48bc69b423e7f808004ec2fe40
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-26 21:02:25 +0800

    deps: prefer `adapter-node` over `adapter-static`

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
     extensions: ['.svelte'],
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter({ fallback: '404.html' }) },
+    kit: { adapter: adapter({ precompress: true }) },
 };
