95328b2ac7e3fcd8e1421e7206436bb04df0e3e5
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-28 07:44:09 +0800

    fix: use dynamic imports for environment variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,13 @@
-import { POSTGRES_DATABASE_URL } from '$lib/server/env/postgres';
-
 import type * as schema from '$lib/server/database/models';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 import { building } from '$app/environment';
 
-const db = building ? null : drizzle<typeof schema>(POSTGRES_DATABASE_URL);
 export async function handle({ event, resolve }) {
-    if (db !== null) event.locals.db = db;
+    if (!building) {
+        // FIXME: Figure out a way to hoist the Drizzle database globally.
+        const { POSTGRES_DATABASE_URL } = await import('$lib/server/env/postgres');
+        event.locals.db = drizzle<typeof schema>(POSTGRES_DATABASE_URL);
+    }
     return await resolve(event);
 }
