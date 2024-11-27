33e682720d3a43459423d85f34812779537f48b7
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-28 07:37:03 +0800

    feat(env): include OAuth2 client secret

--- src/lib/server/env/discord.js
+++ src/lib/server/env/discord.js
@@ -6,10 +6,12 @@ import { env } from '$env/dynamic/private';
 
 if (!building && !dev) {
     assert(typeof env.DISCORD_APPLICATION_ID === 'string', 'missing discord application id');
+    assert(typeof env.DISCORD_OAUTH_SECRET === 'string', 'missing discord oauth secret');
     assert(typeof env.DISCORD_PUBLIC_KEY === 'string', 'missing discord public key');
     assert(typeof env.DISCORD_BOT_TOKEN === 'string', 'missing discord bot token');
 }
 
 export const DISCORD_APPLICATION_ID = env.DISCORD_APPLICATION_ID ?? '';
+export const DISCORD_OAUTH_SECRET = env.DISCORD_OAUTH_SECRET ?? '';
 export const DISCORD_PUBLIC_KEY = Buffer.from(env.DISCORD_PUBLIC_KEY ?? '', 'hex');
 export const DISCORD_BOT_TOKEN = env.DISCORD_BOT_TOKEN ?? '';
