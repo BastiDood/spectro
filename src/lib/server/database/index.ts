519dc1febcc0ea9e21d9ba96f803df75751753e2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-28 21:37:02 +0800

    fix(db): apply the correct schema

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -5,28 +5,28 @@ import type { User } from '$lib/server/models/discord/user';
 
 import { strictEqual } from 'node:assert/strict';
 
-import * as models from './models';
+import * as schema from './models';
 import { lte, sql } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-export const db = drizzle<typeof models>(POSTGRES_DATABASE_URL);
+export const db = drizzle(POSTGRES_DATABASE_URL, { schema });
 
 export type Database = typeof db;
 export type Transaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
 export type Interface = Database | Transaction;
 
-const USER_NAME = sql.raw(models.user.name.name);
-const USER_AVATAR_HASH = sql.raw(models.user.avatarHash.name);
-const USER_UPDATED_AT = sql.raw(models.user.updatedAt.name);
+const USER_NAME = sql.raw(schema.user.name.name);
+const USER_AVATAR_HASH = sql.raw(schema.user.avatarHash.name);
+const USER_UPDATED_AT = sql.raw(schema.user.updatedAt.name);
 
-const GUILD_NAME = sql.raw(models.guild.name.name);
-const GUILD_ICON_HASH = sql.raw(models.guild.iconHash.name);
-const GUILD_SPLASH_HASH = sql.raw(models.guild.splashHash.name);
-const GUILD_UPDATED_AT = sql.raw(models.guild.updatedAt.name);
+const GUILD_NAME = sql.raw(schema.guild.name.name);
+const GUILD_ICON_HASH = sql.raw(schema.guild.iconHash.name);
+const GUILD_SPLASH_HASH = sql.raw(schema.guild.splashHash.name);
+const GUILD_UPDATED_AT = sql.raw(schema.guild.updatedAt.name);
 
 export async function upsertUser(db: Interface, timestamp: Date, user: User) {
     const { rowCount } = await db
-        .insert(models.user)
+        .insert(schema.user)
         .values({
             id: user.id,
             name: user.username,
@@ -35,14 +35,14 @@ export async function upsertUser(db: Interface, timestamp: Date, user: User) {
             updatedAt: timestamp,
         })
         .onConflictDoUpdate({
-            target: models.user.id,
+            target: schema.user.id,
             set: {
                 name: sql`excluded.${USER_NAME}`,
                 avatarHash: sql`excluded.${USER_AVATAR_HASH}`,
                 updatedAt: sql`excluded.${USER_UPDATED_AT}`,
             },
             // Only update with the most recent information.
-            setWhere: lte(models.user.updatedAt, sql`excluded.${USER_UPDATED_AT}`),
+            setWhere: lte(schema.user.updatedAt, sql`excluded.${USER_UPDATED_AT}`),
         });
     strictEqual(rowCount, 1);
 }
@@ -50,7 +50,7 @@ export async function upsertUser(db: Interface, timestamp: Date, user: User) {
 export async function upsertGuild(db: Interface, timestamp: Date, guild: Guild) {
     // FIXME: Discord does not supply all information about the guild.
     await db
-        .insert(models.guild)
+        .insert(schema.guild)
         .values({
             id: guild.id,
             name: guild.name,
@@ -60,7 +60,7 @@ export async function upsertGuild(db: Interface, timestamp: Date, guild: Guild) 
             updatedAt: timestamp,
         })
         .onConflictDoUpdate({
-            target: models.guild.id,
+            target: schema.guild.id,
             set: {
                 name: sql`excluded.${GUILD_NAME}`,
                 iconHash: sql`excluded.${GUILD_ICON_HASH}`,
@@ -68,6 +68,6 @@ export async function upsertGuild(db: Interface, timestamp: Date, guild: Guild) 
                 updatedAt: sql`excluded.${GUILD_UPDATED_AT}`,
             },
             // Only update with the most recent information.
-            setWhere: lte(models.guild.updatedAt, sql`excluded.${GUILD_UPDATED_AT}`),
+            setWhere: lte(schema.guild.updatedAt, sql`excluded.${GUILD_UPDATED_AT}`),
         });
 }
