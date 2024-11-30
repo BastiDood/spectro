eb507e012a1ec8e77ad51916bbbfb38314fe6b07
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-30 19:03:19 +0800

    feat(model): allow buttons in action rows

--- src/lib/server/models/discord/message/component/action-row/index.ts
+++ src/lib/server/models/discord/message/component/action-row/index.ts
@@ -2,9 +2,10 @@ import { type InferOutput, array, literal, object, variant } from 'valibot';
 
 import { MessageComponentType } from '$lib/server/models/discord/message/component/base';
 
+import { MessageComponentButton } from '$lib/server/models/discord/message/component/button';
 import { MessageComponentTextInput } from '$lib/server/models/discord/message/component/text-input';
 
-const MessageComponentInnerComponent = variant('type', [MessageComponentTextInput]);
+const MessageComponentInnerComponent = variant('type', [MessageComponentButton, MessageComponentTextInput]);
 
 export const MessageComponentActionRow = object({
     type: literal(MessageComponentType.ActionRow),
