432e51861a67a8f3115988d38e26d2db27e0dd28
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-30 20:59:36 +0800

    feat(model): add support for more text input fields

--- src/lib/server/models/discord/message/component/text-input/index.ts
+++ src/lib/server/models/discord/message/component/text-input/index.ts
@@ -1,10 +1,34 @@
-import { type InferOutput, literal, object, string } from 'valibot';
+import {
+    type InferOutput,
+    boolean,
+    literal,
+    maxValue,
+    minValue,
+    number,
+    object,
+    optional,
+    picklist,
+    pipe,
+    safeInteger,
+    string,
+} from 'valibot';
 import { MessageComponentType } from '$lib/server/models/discord/message/component/base';
+
+export const enum MessageComponentTextInputStyle {
+    Short = 1,
+    Long = 2,
+}
 
 export const MessageComponentTextInput = object({
     type: literal(MessageComponentType.TextInput),
     custom_id: string(),
-    value: string(),
+    style: picklist([MessageComponentTextInputStyle.Short, MessageComponentTextInputStyle.Long]),
+    label: string(),
+    min_length: optional(pipe(number(), safeInteger(), minValue(0), maxValue(4000))),
+    max_length: optional(pipe(number(), safeInteger(), minValue(1), maxValue(4000))),
+    required: optional(boolean()),
+    value: optional(string()),
+    placeholder: optional(string()),
 });
 
 export type MessageComponentTextInput = InferOutput<typeof MessageComponentTextInput>;
