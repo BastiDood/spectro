c07cc4d5c900b4f08a2fa26e43e306e99a78a03e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-02 05:48:57 +0800

    fix(webhook): reset guild owner permissions on join

--- src/routes/webhook/discord/event/application-authorized.ts
+++ src/routes/webhook/discord/event/application-authorized.ts
@@ -1,11 +1,22 @@
+import assert from 'node:assert/strict';
+
 import type { Database } from '$lib/server/database';
 import type { Snowflake } from '$lib/server/models/discord/snowflake';
 
 import { permission } from '$lib/server/database/models';
-import { strictEqual } from 'node:assert/strict';
+import { sql } from 'drizzle-orm';
 
 export async function handleApplicationAuthorized(db: Database, guildId: Snowflake, userId: Snowflake) {
     // Guild owner must be the first entry into the permission table.
-    const { rowCount } = await db.insert(permission).values({ guildId, userId, isAdmin: true });
-    strictEqual(rowCount, 1);
+    const { rowCount } = await db
+        .insert(permission)
+        .values({ guildId, userId, isAdmin: true })
+        .onConflictDoUpdate({
+            target: [permission.userId, permission.guildId],
+            // FIXME: There is a subtle edge case here where an admin can be demoted,
+            // but if the bot gets re-added into the server (for some reason), then the
+            // server owner gets re-promoted as an administrator. Is this what we want?
+            set: { isAdmin: sql`excluded.${sql.raw(permission.isAdmin.name)}` },
+        });
+    assert(rowCount !== null);
 }
