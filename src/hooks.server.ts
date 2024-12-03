15ebb280049aadcd15398be8b111b47369c9f713
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-04 07:52:26 +0800

    fix: ensure that database methods are not invoked during prerendering

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,18 @@
 import { building } from '$app/environment';
-import { getUserFromSessionId } from '$lib/server/database';
 
 export async function handle({ event, resolve }) {
     if (!building) {
-        const { db } = await import('$lib/server/database');
+        const { db, getUserFromSessionId } = await import('$lib/server/database');
         event.locals.ctx = { db };
 
         const path = event.url.pathname;
         if (path.startsWith('/dashboard/') || path.startsWith('/oauth/discord/')) {
             const sid = event.cookies.get('sid');
-            if (typeof sid !== 'undefined')
+            if (typeof sid !== 'undefined') {
+                event.locals.ctx.session = { sid };
                 // eslint-disable-next-line require-atomic-updates
-                event.locals.ctx.user = await getUserFromSessionId(event.locals.ctx.db, sid);
+                event.locals.ctx.session.user = await getUserFromSessionId(event.locals.ctx.db, sid);
+            }
         }
     }
 
