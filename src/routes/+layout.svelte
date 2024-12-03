b0aedc17b50c2769808e0b7ad4cb3f53d488b661
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-04 07:33:17 +0800

    feat(dashboard): add nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
     import './app.css';
+    const { children } = $props();
+
+    import LogoMark from '$lib/brand/logomark/logomark.svg';
 </script>
 
-<slot />
+<header class="p-2 pr-10">
+    <nav class="navbar">
+        <div class="navbar-start">
+            <a href="/">
+                <img src={LogoMark} alt="Spectro logo" class="w-12" />
+            </a>
+        </div>
+        <div class="navbar-end flex flex-row gap-8">
+            <a href="/docs/" class="link link-hover">Docs</a>
+            <a href="https://github.com/BastiDood/spectro" class="link link-hover">GitHub</a>
+        </div>
+    </nav>
+</header>
+
+{@render children()}
