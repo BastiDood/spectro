ab5a70ab2c15d60d62ef9b6b6780e9ab2b499034
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-04 00:01:25 +0800

    feat(dashboard): add Tailwind theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,4 +2,26 @@ import type { Config } from 'tailwindcss';
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: '#ffff9d',
+                    500: '#f7951d',
+                    950: '#55362e',
+                },
+                secondary: {
+                    50: '#e5a9f5',
+                    500: '#6a367a',
+                    950: '#0a0016',
+                },
+                surface: {
+                    50: '#f7f8f9',
+                    500: '#352f62',
+                    900: '#272544',
+                    950: '#24223d',
+                },
+            },
+        },
+    },
 } satisfies Config;
