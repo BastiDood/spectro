76bd98945181bf8161806a7f716431b03a5cf0b6
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-04 20:34:18 +0800

    fix(model): remove unused `owner` flag

--- src/lib/server/models/discord/guild.ts
+++ src/lib/server/models/discord/guild.ts
@@ -1,10 +1,9 @@
-import { type InferOutput, boolean, nullable, object, string } from 'valibot';
+import { type InferOutput, nullable, object, string } from 'valibot';
 import { Snowflake } from './snowflake';
 
 export const Guild = object({
     id: Snowflake,
     name: string(),
-    owner: boolean(),
     owner_id: Snowflake,
     icon: nullable(string()),
     banner: nullable(string()),
