b68e6d3956b8205701aca5cd234c5098e1168b8d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-05 04:09:32 +0800

    chore: remove `output` configuration from `drizzle.config.js`

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from 'drizzle-kit';
 assert(typeof process.env.POSTGRES_DATABASE_URL !== 'undefined', 'missing postgresql database url');
 
 export default defineConfig({
-    out: 'drizzle',
     dialect: 'postgresql',
     schema: './src/lib/server/database/models/*.ts',
     dbCredentials: { url: process.env.POSTGRES_DATABASE_URL },
