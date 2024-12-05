11871bbf8a0b498d67acf48cd4638b98b5ce0a49
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-06 06:05:18 +0800

    feat(dev): use `pino-pretty` for development

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,8 +1,13 @@
 import { type TransportTargetOptions, pino } from 'pino';
+import { dev } from '$app/environment';
 
 import { AXIOM_DATASET, AXIOM_TOKEN } from '$lib/server/env/axiom';
 
-const targets: TransportTargetOptions[] = [{ target: 'pino/file', options: { destination: 1 } }];
+const targets: TransportTargetOptions[] = [
+    dev ? { target: 'pino-pretty', options: { colorize: 1 } } : { target: 'pino/file', options: { destination: 1 } },
+];
+
+// Axiom
 if (typeof AXIOM_DATASET !== 'undefined' && typeof AXIOM_TOKEN !== 'undefined')
     targets.push({ target: '@axiomhq/pino', options: { dataset: AXIOM_DATASET, token: AXIOM_TOKEN } });
 
