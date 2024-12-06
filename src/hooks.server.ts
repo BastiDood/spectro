20b0d3b71007ad7c09a8cdb51ff3095770139097
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-06 08:35:04 +0800

    feat: include error logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,8 @@
+import { AssertionError } from 'node:assert';
+
 import { building } from '$app/environment';
+
+import { getDotPath, isValiError } from 'valibot';
 
 export async function handle({ event, resolve }) {
     // Do not inject anything when pre-rendering
@@ -31,10 +35,25 @@ export async function handle({ event, resolve }) {
         }
     }
 
-    const start = performance.now();
-    const response = await resolve(event);
-    const requestTimeMillis = performance.now() - start;
+    // eslint-disable-next-line init-declarations
+    let response: Response;
 
+    const start = performance.now();
+    try {
+        response = await resolve(event);
+    } catch (err) {
+        if (isValiError(err)) {
+            const valibotErrorPaths = err.issues.map(issue => getDotPath(issue)).filter(path => path !== null);
+            event.locals.ctx.logger.fatal({ valibotError: err, valibotErrorPaths }, 'valibot validation failed');
+        } else if (err instanceof AssertionError) {
+            event.locals.ctx.logger.fatal({ nodeAssertionError: err }, 'assertion error encountered');
+        } else {
+            event.locals.ctx.logger.fatal(err, 'unknown error encountered');
+        }
+        throw err;
+    }
+
+    const requestTimeMillis = performance.now() - start;
     event.locals.ctx.logger.info({ requestTimeMillis });
     return response;
 }
