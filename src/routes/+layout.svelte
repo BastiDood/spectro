a7551300b23274d264bf299050a94a372615efdc
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-12 19:49:59 +0800

    chore: replace logomark svg in header with png

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 
     import GitHub from '@iconify/icons-simple-icons/github';
     import Icon from '@iconify/svelte';
-    import LogoMark from '$lib/brand/logomark/logomark.svg';
+    import LogoMark from '$lib/brand/logomark/logomark.png';
     import banner from '$lib/brand/link-preview.png?url';
     import favicon from '$lib/brand/favicon.ico?url';
 
