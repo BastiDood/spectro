c0e54d50d87ee2e386daf0c357e3ca9c25e6ecb0
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-14 04:21:32 +0800

    refactor(dashboard): adjust main layout

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,12 +3,12 @@
     import TextLogo from '$lib/brand/logotype/logotype-dark-textonly.svg';
 </script>
 
-<div class="flex flex-col items-center gap-6 p-12 md:gap-8">
+<div class="flex flex-col items-center gap-6 self-center p-12 md:gap-8">
     <div class="flex flex-col justify-center gap-6 md:flex-row md:gap-12 md:py-8">
         <img
             src={Spectro}
             alt="Spectro mascot"
-            class="-z-10 max-h-48 animate-float object-contain drop-shadow-[0_0_32px_rgba(255,255,157,0.35)]"
+            class="max-h-48 animate-float object-contain drop-shadow-[0_0_32px_rgba(255,255,157,0.35)]"
         />
         <img src={TextLogo} alt="Spectro logo" class="max-h-32 object-contain md:max-h-48" />
     </div>
