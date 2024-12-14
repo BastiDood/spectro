bab1dc3b1a1da01f6ec863bfa7386754091c8b8f
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-15 01:02:17 +0800

    feat(dashboard): implement sidebar toggle for mobile

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,28 +1,54 @@
 <script>
+    import Icon from '@iconify/svelte';
+
+    let isSidebarHidden = $state(true);
+
+    function hideSidebar() {
+        isSidebarHidden = true;
+    }
+
     const { children } = $props();
 </script>
 
-<div class="grid grid-cols-[auto_1fr] overflow-y-auto">
+<button
+    class="btn btn-accent fixed left-0 z-10 mt-10 rounded-l-sm px-1 md:hidden"
+    onclick={() => (isSidebarHidden = !isSidebarHidden)}
+>
+    <Icon icon="ic:round-menu" height="24" />
+</button>
+
+<div class="grid grid-cols-[auto_1fr]">
     <div
-        class="md:visible menu h-min sticky top-10 ml-5 flex-nowrap overflow-y-auto rounded-box bg-base-300 p-6 drop-shadow-[3px_3px_0px_rgba(247,149,29,0.35)]"
+        role="none"
+        onclick={hideSidebar}
+        class="{isSidebarHidden ? 'hidden' : ''} fixed inset-0 z-10 bg-base-200 opacity-60"
+    ></div>
+    <div
+        class="{isSidebarHidden
+            ? 'hidden'
+            : ''} absolute top-0 z-20 h-full overflow-y-auto drop-shadow-[3px_3px_0px_rgba(247,149,29,0.35)] md:static md:block md:px-4"
     >
-        <li><a href="#getting-started">Getting Started</a></li>
-        <li>
-            <a href="#basic-usage">Basic Usage</a>
-            <ul>
-                <li><a href="#confess">Confessions</a></li>
-                <li><a href="#reply">Anonymous Replies</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#moderation">Moderation</a>
-            <ul>
-                <li><a href="#channel-setup">Channel Setup</a></li>
-                <li><a href="#manage-confessions">Manage Confessions</a></li>
-            </ul>
-        </li>
+        <div
+            class="menu menu-lg sticky min-h-full flex-nowrap bg-base-300 px-6 py-4 md:menu-md md:top-10 md:min-h-min md:rounded-box"
+        >
+            <li><a href="#getting-started" onclick={hideSidebar}>Getting Started</a></li>
+            <li>
+                <a href="#basic-usage" onclick={hideSidebar}>Basic Usage</a>
+                <ul>
+                    <li><a href="#confess" onclick={hideSidebar}>Confessions</a></li>
+                    <li><a href="#reply" onclick={hideSidebar}>Anonymous Replies</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#moderation" onclick={hideSidebar}>Moderation</a>
+                <ul>
+                    <li><a href="#channel-setup" onclick={hideSidebar}>Channel Setup</a></li>
+                    <li><a href="#manage-confessions" onclick={hideSidebar}>Manage Confessions</a></li>
+                </ul>
+            </li>
+        </div>
     </div>
-    <div>
+    <div class="overflow-y-auto">
         {@render children()}
     </div>
 </div>
