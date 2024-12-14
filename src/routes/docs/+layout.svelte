44f46f0229095e83bbf4148de852fe04a0cf5765
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-14 19:29:56 +0800

    feat(dashboard): implement table of contents for docs page

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -7,9 +7,22 @@
         class="menu sticky top-10 m-10 w-2/5 flex-nowrap overflow-y-auto rounded-box bg-base-300 p-6 drop-shadow-[3px_3px_0px_rgba(247,149,29,0.35)]"
     >
         <li class="menu-title text-secondary-content">Spectro Docs</li>
-        <li><a href="/">Getting Started</a></li>
-        <li><a href="/">Basic Usage</a></li>
-        <li><a href="/">Moderation</a></li>
+        <li><a href="#features">Features</a></li>
+        <li><a href="#getting-started">Getting Started</a></li>
+        <li>
+            <a href="#basic-usage">Basic Usage</a>
+            <ul>
+                <li><a href="#confess">Confessions</a></li>
+                <li><a href="#reply">Anonymous Replies</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#moderation">Moderation</a>
+            <ul>
+                <li><a href="#channel-setup">Channel Setup</a></li>
+                <li><a href="#manage-confessions">Manage Confessions</a></li>
+            </ul>
+        </li>
     </div>
     <div>
         {@render children()}
