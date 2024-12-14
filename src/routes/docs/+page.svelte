7a7cadaf20425bee8a7c4b61fb05dba819e909fa
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-15 00:53:41 +0800

    feat(dashboard): adjust alignments for mobile

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -21,21 +21,31 @@
         the bot.
     </p>
     <h3>Using Spectro</h3>
-    <code class="rounded-md bg-base-300 px-4 py-2 text-lg text-primary drop-shadow-md"
-        >/help <span class="badge badge-neutral tooltip tooltip-accent" data-tip="Message visibility: public (optional)"
-            >preview</span
-        ></code
-    >
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
+        <code class="rounded-md bg-base-300 px-4 py-2 text-lg text-primary drop-shadow-md"
+            >/help <div
+                class="badge badge-neutral tooltip tooltip-accent"
+                data-tip="Message visibility: public (optional)"
+            >
+                preview
+            </div></code
+        >
+    </div>
     <p class="mb-10">
         <strong>Open the help page to show a list of commands.</strong> By default, the help page is shown privately,
         but you can enable the
         <code>public</code> message mode. This command can be run anywhere: server channels, private DMs, etc.
     </p>
-    <code class="rounded-md bg-base-300 px-4 py-2 text-lg text-primary drop-shadow-md"
-        >/info <span class="badge badge-neutral tooltip tooltip-accent" data-tip="Message visibility: public (optional)"
-            >preview</span
-        ></code
-    >
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
+        <code class="rounded-md bg-base-300 px-4 py-2 text-lg text-primary drop-shadow-md"
+            >/info <div
+                class="badge badge-neutral tooltip tooltip-accent"
+                data-tip="Message visibility: public (optional)"
+            >
+                preview
+            </div></code
+        >
+    </div>
     <p class="mb-10">
         <strong>View important information and links about Spectro,</strong> including links for reporting bugs and
         viewing the source code. By default, the information page is shown privately, but you can enable the
@@ -48,24 +58,26 @@
     </p>
 
     <h2 id="basic-usage">Basic Usage</h2>
-    <div>
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
         <code id="confess" class="scroll-mt-10 rounded-md bg-base-300 px-3 py-1.5 text-lg text-primary drop-shadow-md"
-            >/confess <span
+            >/confess <div
                 class="badge badge-secondary tooltip tooltip-accent"
-                data-tip="Content of the confession message">content</span
-            ></code
+                data-tip="Content of the confession message"
+            >
+                content
+            </div></code
         >
-        <span class="badge badge-info badge-outline ml-2">Required permissions: Send Messages</span>
+        <div class="badge badge-info badge-outline place-self-center">Required permissions: Send Messages</div>
     </div>
     <p class="mb-10">
         <strong>Send a confession to the current channel.</strong> This command fails if the current channel has not yet
         been configured to receive confessions.
     </p>
-    <div>
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
         <code id="reply" class="scroll-mt-10 rounded-md bg-base-300 px-3 py-1.5 text-lg text-primary drop-shadow-md"
             >Apps &gt; Reply Anonymously</code
         >
-        <span class="badge badge-info badge-outline ml-2">Required permissions: Send Messages</span>
+        <div class="badge badge-info badge-outline place-self-center">Required permissions: Send Messages</div>
     </div>
     <p class="mb-10">
         <strong>Anonymously reply</strong> to any message (in a confessions-enabled channel) by
@@ -76,27 +88,35 @@
     <h2 id="moderation">Moderation</h2>
     <h3 id="channel-setup" class="scroll-mt-10">Channel Setup</h3>
     <div></div>
-    <div>
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
         <code class="rounded-md bg-base-300 px-3 py-1.5 text-lg text-primary drop-shadow-md"
             >/setup
-            <span
+            <div
                 class="badge badge-secondary tooltip tooltip-accent"
-                data-tip="Moderator-only channel for confession logs">channel</span
+                data-tip="Moderator-only channel for confession logs"
             >
-            <span
+                channel
+            </div>
+            <div
                 class="badge badge-neutral tooltip tooltip-accent"
-                data-tip="Custom title for confession messages (optional)">label</span
+                data-tip="Custom title for confession messages (optional)"
             >
-            <span
+                label
+            </div>
+            <div
                 class="badge badge-neutral tooltip tooltip-accent"
-                data-tip="Custom hex color for confession messages (optional)">color</span
+                data-tip="Custom hex color for confession messages (optional)"
             >
-            <span
+                color
+            </div>
+            <div
                 class="badge badge-neutral tooltip tooltip-accent"
-                data-tip="Require approvals: true or false (default)">approval</span
+                data-tip="Require approvals: true or false (default)"
             >
+                approval
+            </div>
         </code>
-        <span class="badge badge-info badge-outline ml-2">Required permissions: Manage Channels</span>
+        <div class="badge badge-info badge-outline place-self-center">Required permissions: Manage Channels</div>
     </div>
     <p>
         <strong>Enable confessions for the current channel where the command is being run.</strong> All confessions,
@@ -114,21 +134,21 @@
     </p>
 
     <h3 id="manage-confessions" class="scroll-mt-10">Manage Confessions</h3>
-    <div>
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
         <code class="rounded-md bg-base-300 px-3 py-1.5 text-lg text-primary drop-shadow-md">/lockdown</code>
-        <span class="badge badge-info badge-outline ml-2">Required permissions: Manage Channels</span>
+        <div class="badge badge-info badge-outline place-self-center">Required permissions: Manage Channels</div>
     </div>
     <p class="mb-10">
         <strong>Temporarily disable anonymous confessions for the current channel.</strong> Previous settings are
         preserved for the next time <code>/setup</code> is run.
     </p>
-    <div>
+    <div class="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
         <code class="rounded-md bg-base-300 px-3 py-1.5 text-lg text-primary drop-shadow-md"
-            >/resend <span class="badge badge-secondary tooltip tooltip-accent" data-tip="ID of confession to resend"
-                >id</span
-            ></code
+            >/resend <div class="badge badge-secondary tooltip tooltip-accent" data-tip="ID of confession to resend">
+                id
+            </div></code
         >
-        <span class="badge badge-info badge-outline ml-2">Required permissions: Manage Messages</span>
+        <div class="badge badge-info badge-outline place-self-center">Required permissions: Manage Messages</div>
     </div>
     <p class="mb-10">
         <strong>Resend an existing confession by its <code>id</code>.</strong> This is useful for times when a confession
