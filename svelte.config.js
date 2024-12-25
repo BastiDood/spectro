1583b1c4c4880e761b5a78085b7249bf7149d95a
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-12-25 12:22:10 +0800

    refactor: prefer JSDoc type imports

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
+/** @import { Config } from '@sveltejs/kit' */
+
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {Config} */
 export default {
-    extensions: ['.svelte'],
     preprocess: vitePreprocess(),
     kit: { adapter: adapter({ precompress: true }) },
 };
