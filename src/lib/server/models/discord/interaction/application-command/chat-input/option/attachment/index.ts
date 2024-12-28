584cc2b4692184655958edb5289af13697a4097a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-28 22:28:07 +0800

    feat: add expected Snowflake to OptionAttachment model

--- src/lib/server/models/discord/interaction/application-command/chat-input/option/attachment/index.ts
+++ src/lib/server/models/discord/interaction/application-command/chat-input/option/attachment/index.ts
@@ -4,10 +4,12 @@ import {
     InteractionApplicationCommandChatInputOptionBase,
     InteractionApplicationCommandChatInputOptionType,
 } from '$lib/server/models/discord/interaction/application-command/chat-input/option/base';
+import { Snowflake } from '$lib/server/models/discord/snowflake';
 
 export const InteractionApplicationCommandChatInputOptionAttachment = object({
     ...InteractionApplicationCommandChatInputOptionBase.entries,
     type: literal(InteractionApplicationCommandChatInputOptionType.Attachment),
+    value: Snowflake
 });
 
 export type InteractionApplicationCommandChatInputOptionAttachment = InferOutput<
