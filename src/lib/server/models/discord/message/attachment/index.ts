c9e191b7944d46491a80b017626f485732ae7e6e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-28 22:18:28 +0800

    feat: define model for discord attachments

---
+++ src/lib/server/models/discord/message/attachment/index.ts
@@ -0,0 +1,17 @@
+import { type InferOutput, number, object, optional, string } from "valibot";
+import { Snowflake } from '$lib/server/models/discord/snowflake';
+
+export const Attachment = object({
+    id: Snowflake,
+    filename: string(),
+    title: optional(string()),
+    description: optional(string()),
+    content_type: optional(string()),
+    size: optional(number()),
+    url: string(),
+    proxy_url: string(),
+    height: optional(number()),
+    width: optional(number()) 
+})
+
+export type Attachment = InferOutput<typeof Attachment>;
