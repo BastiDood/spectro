d38580501180e6c21af0857806019b14c2b1175b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-30 17:41:58 +0800

    feat: add migration artifacts

---
+++ drizzle/0007_last_tombstone.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "app"."confession" ADD COLUMN "attachment_url" text;--> statement-breakpoint
+ALTER TABLE "app"."confession" ADD COLUMN "attachment_filename" text;--> statement-breakpoint
+ALTER TABLE "app"."confession" ADD COLUMN "attachment_type" text;
