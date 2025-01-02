204a2e7985a5ccbbbf440ff1bbff487de203d1b7
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-01-02 15:21:47 +0800

    feat(seo): add privacy policy to sitemap

--- src/routes/(seo)/robots.txt/+server.js
+++ src/routes/(seo)/robots.txt/+server.js
@@ -3,6 +3,6 @@ import { ORIGIN } from '$lib/env';
 export const prerender = true;
 
 export function GET() {
-    const lines = ['User-agent: *', 'Allow: /', '', `Sitemap: ${ORIGIN.origin}/sitemaps.xml`];
+    const lines = ['User-agent: *', 'Allow: /', '', `Sitemap: ${ORIGIN.origin}/sitemap.xml`];
     return new Response(lines.join('\n'));
 }
