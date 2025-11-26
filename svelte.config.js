78007da63dea1e4d86070cbbd8a78959a94d9fae
Basti Ortiz <ortiz@bastidood.dev>
2025-11-27 05:24:37 +0800

    chore: remove unused dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 /** @import { Config } from '@sveltejs/kit'; */
 
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import pkg from './package.json' with { type: 'json' };
 
 /** @type {Config} */
 export default {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({ precompress: true }),
     version: { name: pkg.version },
