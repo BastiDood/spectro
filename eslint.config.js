6d0e59301dd62e9b036a779289736691b1f3408e
Basti Ortiz <ortiz@bastidood.dev>
2025-12-12 09:55:37 +0800

    chore: migrate to Vercel

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ import { defineConfig } from 'eslint/config';
 import svelteConfig from './svelte.config.js';
 
 export default defineConfig(
-  { ignores: ['node_modules/**/*', '.svelte-kit/**/*', '.wrangler/**/*'] },
+  { ignores: ['node_modules/**/*', '.svelte-kit/**/*', '.vercel/**/*'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
     ...html.configs['flat/recommended'],
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 /** @import { Config } from '@sveltejs/kit'; */
 
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {Config} */
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ regions: ['sin1'], runtime: 'nodejs24.x', memory: 128 }),
     experimental: {
       tracing: { server: true },
       instrumentation: { server: true },
