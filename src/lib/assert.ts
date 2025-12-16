3c29efdd61885cea552dc625cac223aaf4e48983
Basti Ortiz <ortiz@bastidood.dev>
2025-12-17 04:25:54 +0800

    chore: use better assertion ergonomics

--- src/lib/assert.ts
+++ src/lib/assert.ts
@@ -3,33 +3,45 @@ import { Logger } from '$lib/server/telemetry/logger';
 const SERVICE_NAME = 'lib.assert';
 const logger = new Logger(SERVICE_NAME);
 
+export class UnreachableCodeError extends Error {
+  constructor() {
+    super('unreachable code');
+    this.name = 'UnreachableCodeError';
+  }
+
+  static throwNew(): never {
+    const error = new UnreachableCodeError();
+    logger.error(error.message, error);
+    throw error;
+  }
+}
+
 export class AssertionError extends Error {
-  constructor(message?: string) {
+  constructor(message = 'assertion failed') {
     super(message);
     this.name = 'AssertionError';
   }
+
+  static throwNew(message?: string): never {
+    const error = new AssertionError(message);
+    logger.error(error.message, error);
+    throw error;
+  }
 }
 
+/** @throws {AssertionError} */
 export function assertDefined<T>(value?: T | undefined) {
-  if (typeof value === 'undefined') {
-    const error = new AssertionError('value must be defined');
-    logger.error('value must be defined', error);
-    throw error;
-  }
+  if (typeof value === 'undefined') AssertionError.throwNew('value must be defined');
   return value;
 }
 
+/** @throws {AssertionError} */
 export function assertOptional<T>([value, ...values]: T[]) {
-  if (values.length > 0) {
-    const error = new AssertionError('expected at most one value');
-    logger.error('expected at most one value', error, {
-      'values.length': values.length,
-    });
-    throw error;
-  }
+  if (values.length > 0) AssertionError.throwNew('expected at most one value');
   return value;
 }
 
+/** @throws {AssertionError} */
 export function assertSingle<T>(values: T[]) {
   return assertDefined(assertOptional(values));
 }
