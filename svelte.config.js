8bb9e870d05616542840629fb98c00247f718e3e
Basti Ortiz <ortiz@bastidood.dev>
2025-12-16 08:29:21 +0800

    chore: move function region to `iad1`

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {Config} */
 export default {
   kit: {
-    adapter: adapter({ regions: ['sin1'], runtime: 'nodejs24.x', memory: 128 }),
+    adapter: adapter({ regions: ['iad1'], runtime: 'nodejs24.x', memory: 128 }),
     experimental: {
       tracing: { server: true },
       instrumentation: { server: true },
